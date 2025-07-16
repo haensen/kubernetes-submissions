bee3875dca8c6918132021298e009afb576e75ee
Hannes Haimilahti <hhaimilahti@gmail.com>
2025-07-16 18:16:36 +0300

    Exercise 1.7

--- log_output/README.md
+++ log_output/README.md
@@ -1,4 +1,7 @@
 # Log output app
 
 Deploy with
-```kubectl apply -f manifests/deployment.yaml```
+```kubectl apply -f manifests```
+
+Endpoint GET / returns the current timestamp and the random string.
+The port is the same as was mapped to 80 on load balancer, probably 8081.
--- log_output/main.ts
+++ log_output/main.ts
@@ -1,3 +1,5 @@
+import { Hono } from "@hono/hono";
+
 const createRandomString = () => {
     return Math.random().toString(36).substring(2);
 }
@@ -7,3 +9,15 @@ const randomString = createRandomString();
 setInterval(() => {
     console.log(`${new Date().toISOString()}: ${randomString}`);
 }, 5000);
+
+const app = new Hono();
+
+app.get("/", (c) => c.text(`${new Date().toISOString()}: ${randomString}`));
+
+const PORT = Deno.env.get("PORT") || 8000;
+Deno.serve({
+    port: PORT,
+    onListen: ({ port }) => {
+        console.log(`Server started in port ${port}`);
+    }
+}, app.fetch);

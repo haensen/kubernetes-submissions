189286c06b041666b2a614a49f778e1728a890cb
Hannes Haimilahti <hhaimilahti@gmail.com>
2025-07-17 14:11:28 +0300

    Exercise 2.8

--- project/todo_backend/main.ts
+++ project/todo_backend/main.ts
@@ -1,18 +1,21 @@
 import { Hono } from "@hono/hono";
 import { cors } from "jsr:@hono/hono@4.6.5/cors";
+import postgres from "postgres";
+
+const sql = postgres();
+
+await sql`CREATE TABLE IF NOT EXISTS todos (id SERIAL PRIMARY KEY, description TEXT)`;
 
 const app = new Hono();
 app.use("*", cors());
 
-let todos = [];
-
 app.get("/todos", async (c) => {
-    return c.json(todos);
+    return c.json(await sql`SELECT description FROM todos`);
 });
 
 app.post("/todos", async (c) => {
     const todo = await c.req.json();
-    todos.push(todo);
+    await sql`INSERT INTO todos (description) VALUES (${todo.description})`;
     return c.json(todo);
 });
 
